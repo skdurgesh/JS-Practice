210cf79e8d110dd3032f515afa96d67a0b2590fc
Durgesh Singh <skdurgesh@gmail.com>
2025-06-08 15:09:17 +0530

    all js files changes got added.

--- js/function.js
+++ js/function.js
@@ -10,7 +10,7 @@ function myNewFunc() {
 myNewFunc();
 myNewFunc(); */
 
-function fruitProcessor(apples, kiwis){
+/* function fruitProcessor(apples, kiwis){
     //console.log(apples, kiwis);
     const juice = `Juice with ${apples} apples and ${kiwis} kiwis.`;
     return juice;
@@ -25,4 +25,147 @@ function salaryCalculation(salary, bonus){
 }
 
 let salaryCount = salaryCalculation(25000, 5000);
-console.log(salaryCount);
+console.log(salaryCount); */
+
+// Function Declaration
+/* function calcAge1(birthYeah){
+    return 2037 - birthYeah; 
+}
+const age1 = calcAge1(1985); */
+
+// Function Expression
+/* const calcAge2 = function(birthYeah){
+    return 2037 - birthYeah;
+}
+const age2 = calcAge2(1993);
+console.log(age1, age2); */
+
+// spread/Rest Operator
+
+/* function calculateCartPrice(val1, val2, ...num1){
+    return num1;
+}
+console.log(calculateCartPrice(200, 400, 500, 1000)); */
+
+// one way to decalre the object
+/* const user = {
+    username: "Durgesh Singh",
+    price: 399
+}
+
+function handleObject(someObject){
+    console.log(`Username is ${someObject.username} and price is ${someObject.price}`);
+}
+
+//handleObject(user);
+// You call the object within on the time of function invoking. 
+// another way to call the object.
+handleObject({
+    username: "Sam",
+    price: 399
+}); */
+
+// calling the Array into function
+/* const myNewArray = [200, 400, 100, 1000];
+function returnSecondValue(getArray){
+    return getArray[0];
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 1000])); */
+
+// Function Scopes
+
+/* let a = 10;
+
+{
+    console.log(a);
+    let scopeVar = 20;
+}
+{
+    let x = 1000;
+    console.log(scopeVar);
+} */
+//console.log(scopeVar);
+
+// Closure
+/* function one(){
+    const username = "Durgesh";
+
+    function two() {
+        const website = "http://www.amazon.in";
+        console.log(username);
+        console.log(website);
+    }
+    two();
+}
+one(); */
+
+// Arrow function and this keyword 
+/* const yearsUntilRetirement = (birthYeah, firstName) => {
+    const age = 2025 - birthYeah;
+    const retirement = 60 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1982, "Anand"));
+console.log(yearsUntilRetirement(1985, "Durgesh")); */
+
+/* const user = {
+    username: "Durgesh",
+    price: 499,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website.`);
+        console.log(this);
+    }
+} 
+
+//user.welcomeMessage();
+//user.username = "Sam";
+//user.welcomeMessage();
+
+//console.log(this);*/
+
+/* function chaicode(){
+    const username = "Durgesh";
+    console.log(this.username);
+}
+
+chaicode(); */
+
+/* const chaicode = function(){
+    let username = "Anand";
+    console.log(this.username);
+}
+chaicode(); */
+
+/* const chaicode = () => {
+     let username = "Anand";
+    console.log(this.username);
+}
+chaicode(); */
+
+/* const addTwo = (num1, num2) =>{
+    return num1 + num2;
+}
+console.log(addTwo(4,5)); */
+
+/* const addTwo = (num1, num2) => (num1 + num2);    // implicit type
+console.log(addTwo(4,5)); */
+
+// const itemArray = [3, 5, 90, 589];
+// itemArray.forEach()
+
+// Immediately Invoked Function Expression (IIFE)
+
+(function dbConnect(){
+    // named IIFE
+    console.log(`DB Connected.`);
+})();
+
+((dbname) => {
+    // unnamed IIFE
+    console.log(`DB Connected again to ${dbname}`);
+})("Durgesh");
